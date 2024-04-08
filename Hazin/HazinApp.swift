1434ad1a69e0ded645a07ef2f0c1e07f7dd79bf7
Maraj Hossain <marajhossaincse@gmail.com>
2024-04-08 22:00:51 +0600

    Initial commit.

---
+++ Hazin/HazinApp.swift
@@ -0,0 +1,17 @@
+//
+//  HazinApp.swift
+//  Hazin
+//
+//  Created by Maraj Hossain on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HazinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
