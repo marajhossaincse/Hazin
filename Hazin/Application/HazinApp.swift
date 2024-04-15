1cd925549233789e6aa08e7e6473bcf7d47bc32d
Maraj Hossain <marajhossaincse@gmail.com>
2024-04-15 18:51:54 +0600

    Refactor code.

--- Hazin/Application/HazinApp.swift
+++ Hazin/Application/HazinApp.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct HazinApp: App {
     var body: some Scene {
         WindowGroup {
-//            ContentView()
-            HomeView()
+            ContentView()
         }
     }
 }
