de69e144ff48c450291d8751096c905720283c00
Maraj Hossain <marajhossaincse@gmail.com>
2024-04-16 20:14:30 +0600

    Implement hero image view.

--- Hazin/Views/MainViews/HomeView.swift
+++ Hazin/Views/MainViews/HomeView.swift
@@ -11,16 +11,43 @@ struct HomeView: View {
     var body: some View {
         ZStack {
             Color.white.edgesIgnoringSafeArea(.all)
-            ZStack {}
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .overlay(alignment: .top) {
-                    HeaderView {} cartAction: {}
+            ZStack {
+                VStack(spacing: 0) {
+                    ScrollView(.vertical) {
+                        HeroImageView()
+                    }
                 }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .overlay(alignment: .top) {
+                HeaderView {} cartAction: {}
+            }
         }
     }
     
     @ViewBuilder
-    private func HeroImageView() -> some View {}
+    private func HeroImageView() -> some View {
+        ZStack {
+            Image("Hero")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: .infinity)
+                .frame(height: 620)
+            
+            Button {
+                print("Explore Collection Button Clicked.")
+            } label: {
+                RoundedRectangle(cornerRadius: 30).overlay {
+                    Text("Explore Collection")
+                        .font(Font.custom("Tenor Sans", size: 20))
+                        .foregroundColor(.white)
+                }
+                .frame(width: 253, height: 40)
+                .tint(.black.opacity(0.4))
+                .offset(.init(width: 0, height: 250))
+            }
+        }
+    }
     
     @ViewBuilder
     private func NewArrivalView() -> some View {}
