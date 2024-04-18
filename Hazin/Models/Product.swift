39aa9ce255c272ffa85ed41e845a968b3d58a15a
Maraj Hossain <marajhossaincse@gmail.com>
2024-04-18 12:19:07 +0600

    Add model for product.

---
+++ Hazin/Models/Product.swift
@@ -0,0 +1,17 @@
+//
+//  Product.swift
+//  Hazin
+//
+//  Created by Maraj Hossain on 18/4/24.
+//
+
+import Foundation
+
+struct Product{
+    var title: String
+    var price: Int
+    var description: String
+    var images: [String]
+//    var category: Categories
+//    var gender: GenderCategories
+}
